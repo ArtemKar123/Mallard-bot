fe211f085fec9cb372c35bac483d8da30c602193
ArtemKar123 <wartemtem@yandex.ru>
2022-05-25 16:13:38 +0300

    denial of миу

--- mallard.py
+++ mallard.py
@@ -1,5 +1,5 @@
 import random
-from content.dictionaries import BASIC_REPLIES_DICT
+from content.dictionaries import BASIC_REPLIES_DICT, DENIAL_REPLIES_DICT
 
 
 class Mallard:
@@ -16,21 +16,24 @@ class Mallard:
         """
         if len(saying) == 0:
             return None
-        if (reply := self.check_simple_sayings(saying)) is not None:
+        if (reply := self.check_basic_saying_based_on_dict(saying, BASIC_REPLIES_DICT)) is not None:
+            return reply
+        if (reply := self.check_basic_saying_based_on_dict(saying, DENIAL_REPLIES_DICT)) is not None:
             return reply
 
-    def check_simple_sayings(self, saying: str):
+    @staticmethod
+    def check_basic_saying_based_on_dict(saying: str, basic_dict):
         """
         Checks if saying has some basic words to reply (like "ква").
         :param saying:
         """
         saying = saying.upper()
         found_keywords = []
-        for keyword in BASIC_REPLIES_DICT.keys():
-            if (upper := keyword.upper()) in saying and len(BASIC_REPLIES_DICT[keyword]) > 0:
+        for keyword in basic_dict.keys():
+            if (upper := keyword.upper()) in saying and len(basic_dict[keyword]) > 0:
                 found_keywords.append(upper)
 
         if (l := len(found_keywords)) > 0:
             chosen_keyword = found_keywords[random.randint(0, l - 1)]
-            return BASIC_REPLIES_DICT[chosen_keyword][random.randint(0, len(BASIC_REPLIES_DICT[chosen_keyword]) - 1)]
+            return basic_dict[chosen_keyword][random.randint(0, len(basic_dict[chosen_keyword]) - 1)]
         return None
--- tests.py
+++ tests.py
@@ -1,23 +1,34 @@
 from mallard import Mallard
-from content.dictionaries import BASIC_REPLIES_DICT
+from content.dictionaries import BASIC_REPLIES_DICT, DENIAL_REPLIES_DICT
+
+mallard = Mallard()
 
 
-def basic_reply_is_valid(reply: str, keyword):
-    return reply is not None and reply in BASIC_REPLIES_DICT[keyword.upper()]
+def basic_reply_is_valid(reply: str, keyword, desired_dict=BASIC_REPLIES_DICT):
+    return reply is not None and reply in desired_dict[keyword.upper()]
 
 
 def test_basic_answers():
-    mallard = Mallard()
+    assert basic_reply_is_valid(mallard.process('ква'), 'КВА')
+    assert basic_reply_is_valid(mallard.process('КвА'), 'КВА')
+    assert basic_reply_is_valid(mallard.process('квашеная капуста'), 'КВА')
 
-    assert basic_reply_is_valid(mallard.check_simple_sayings('ква'), 'КВА')
-    assert basic_reply_is_valid(mallard.check_simple_sayings('КвА'), 'КВА')
-    assert basic_reply_is_valid(mallard.check_simple_sayings('квашеная капуста'), 'КВА')
-
-    double_rep = mallard.check_simple_sayings('кряква')
+    double_rep = mallard.process('кряква')
     assert basic_reply_is_valid(double_rep, 'КВА') or basic_reply_is_valid(double_rep, 'КРЯ')
 
-    double_rep = mallard.check_simple_sayings('квакря')
+    double_rep = mallard.process('квакря')
     assert basic_reply_is_valid(double_rep, 'КВА') or basic_reply_is_valid(double_rep, 'КРЯ')
 
-    assert mallard.check_simple_sayings('абоба') is None
-    assert mallard.check_simple_sayings('к в а') is None
+    assert mallard.process('абоба') is None
+    assert mallard.process('к в а') is None
+
+
+def test_basic_order():
+    assert basic_reply_is_valid(mallard.process('ква миу'), 'КВА')
+    assert basic_reply_is_valid(mallard.process('миуква'), 'КВА')
+
+
+def test_denial_answers():
+    assert basic_reply_is_valid(mallard.process('миу'), 'МИУ', DENIAL_REPLIES_DICT)
+    assert basic_reply_is_valid(mallard.process('миУ'), 'МИУ', DENIAL_REPLIES_DICT)
+    assert mallard.process('миy') is None
