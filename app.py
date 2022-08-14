49bbd1a34aee7ee451c470693396e9f911ec0e50
ArtemKar123 <wartemtem@yandex.ru>
2022-08-15 00:13:06 +0300

    trying to make videos

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 import uuid
+from io import BytesIO
+
+import cv2
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import Updater
 from telegram.ext import MessageHandler, Filters
 from mallard import Mallard
 from stickers import file2sticker, FilePreprocessType
+import tempfile
 
 mallard = Mallard(random_answer_rate=250)
 
@@ -30,28 +34,57 @@ def echo(update: Update, context: CallbackContext):
 def command(update: Update, context: CallbackContext):
     if update.message.text == '/qwa':
         quote(update, context)
+    if update.message.text == '/krya':
+        video_quote(update, context)
 
 
-def quote(update: Update, context: CallbackContext):
+def video_quote(update: Update, context: CallbackContext):
     message = update.message
-    # print(message)
-    if (False):  # TODO: check if quotly is in chat
-        pass
-    else:
+
+    file_id = None
+    if (original_message := message.reply_to_message) is not None:
+        if (
+                video := original_message.document if original_message.document is not None else original_message.video) is not None:  # circle video
+            if video.file_size > 10485760:  # 10mb
+                return
+            file_id = video.file_id
+
+    file_bytes = context.bot.getFile(file_id).download_as_bytearray()
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(file_bytes)
+
+        cap = cv2.VideoCapture(temp.name)
+        w = int(cap.get(3))  # float `width`
+        h = int(cap.get(4))
+        if w >= h:
+            new_h = int(h * (512 / w))
+            new_w = 512
+        else:
+            new_w = int(w * (512 / h))
+            new_h = 512
+
+        print(new_w, new_h)
+        # cap.set(3, new_w)
+        # cap.set(4, new_h)
+
+        # fourcc = cv2.VideoWriter_fourcc(*'WEBM')
         sticker = None
-        if (original_message := message.reply_to_message) is not None:
-            if (video_note := original_message.video_note) is not None:  # circle video
-                if (thumb := video_note.thumb) is not None:
-                    if (file_id := thumb.file_id) is not None:
-                        sticker = file2sticker(file_id, context, preprocess_type=FilePreprocessType.circle)
-            elif (
-                    video := original_message.document if original_message.document is not None else original_message.video) is not None:  # circle video
-                if (thumb := video.thumb) is not None:
-                    if (file_id := thumb.file_id) is not None:
-                        sticker = file2sticker(file_id, context)
-            elif (photo := original_message.photo) is not None and len(photo) > 0:
-                if (file_id := photo[-1].file_id) is not None:
-                    sticker = file2sticker(file_id, context)
+        with tempfile.NamedTemporaryFile() as out_temp:
+
+            fourcc = cv2.VideoWriter_fourcc(*'vp80')
+            out = cv2.VideoWriter(out_temp.name, fourcc, 20.0, (new_w, new_h))
+            while True:
+                # print('a')
+                ret, frame = cap.read()
+                if not ret:
+                    break
+                frame = cv2.resize(frame, (new_w, new_h))
+                out.write(frame)
+
+            out.release()
+            cap.release()
+            sticker = BytesIO(out_temp.read())
+            sticker.seek(0)
         if sticker is None:
             return
         uid = str(uuid.uuid4())
@@ -59,11 +92,49 @@ def quote(update: Update, context: CallbackContext):
         sticker_set_name = f"s{uid}_by_cryakwa_bot"
         # print(message.from_user.id)
         context.bot.create_new_sticker_set(user_id=admin_id, name=sticker_set_name, title="Sticker by @cryakwa_bot",
-                                           png_sticker=sticker,
+                                           webm_sticker=sticker,
                                            emojis="\U0001F60C")
         sticker_set = context.bot.get_sticker_set(sticker_set_name)
         update.message.reply_sticker(reply_to_message_id=update.effective_message.message_id,
                                      sticker=sticker_set.stickers[-1])
+
+    pass
+
+
+def quote(update: Update, context: CallbackContext):
+    message = update.message
+    print(message)
+    sticker = None
+    if (original_message := message.reply_to_message) is not None:
+        if (video_note := original_message.video_note) is not None:  # circle video
+            if (thumb := video_note.thumb) is not None:
+                if (file_id := thumb.file_id) is not None:
+                    sticker = file2sticker(file_id, context, preprocess_type=FilePreprocessType.circle)
+        elif (
+                video := original_message.document if original_message.document is not None else original_message.video) is not None:  # circle video
+            if (thumb := video.thumb) is not None:
+                if (file_id := thumb.file_id) is not None:
+                    sticker = file2sticker(file_id, context)
+            else:
+                if video.file_size > 10485760:  # 10mb
+                    return
+                sticker = file2sticker(video.file_id, context, preprocess_type=FilePreprocessType.video_thumb)
+
+        elif (photo := original_message.photo) is not None and len(photo) > 0:
+            if (file_id := photo[-1].file_id) is not None:
+                sticker = file2sticker(file_id, context)
+    if sticker is None:
+        return
+    uid = str(uuid.uuid4())
+    uid = uid.replace("-", "")
+    sticker_set_name = f"s{uid}_by_krya_test_bot"
+    # print(message.from_user.id)
+    context.bot.create_new_sticker_set(user_id=admin_id, name=sticker_set_name, title="Sticker by @cryakwa_bot",
+                                       png_sticker=sticker,
+                                       emojis="\U0001F60C")
+    sticker_set = context.bot.get_sticker_set(sticker_set_name)
+    update.message.reply_sticker(reply_to_message_id=update.effective_message.message_id,
+                                 sticker=sticker_set.stickers[-1])
 
 
 def main():
--- stickers.py
+++ stickers.py
@@ -3,25 +3,72 @@ from telegram.ext import CallbackContext
 import cv2
 import numpy as np
 from io import BytesIO
+import tempfile
 
 
 class FilePreprocessType(enum.Enum):
-    none = 0,
+    none = 0
     circle = 1
-    default = 2
+    video_thumb = 2
+    animation = 3
+    default = 4
+
+
+def file2animated_sticker(file_id: str, context: CallbackContext):
+    file_bytes = context.bot.getFile(file_id).download_as_bytearray()
+
+    with tempfile.NamedTemporaryFile() as temp:
+        temp.write(file_bytes)
+
+        cap = cv2.VideoCapture(temp.name)
+        w = int(cap.get(3))  # float `width`
+        h = int(cap.get(4))
+        if w >= h:
+            new_h = int(h * (512 / w))
+            new_w = 512
+        else:
+            new_w = int(w * (512 / h))
+            new_h = 512
+
+        cap.set(3, new_w)
+        cap.set(4, new_h)
+
+        fourcc = cv2.VideoWriter_fourcc(*'WEBM')
+        with tempfile.NamedTemporaryFile() as out_temp:
+            out = cv2.VideoWriter(out_temp, fourcc, 14.0, (new_w, new_h))
+            while True:
+                ret, frame = cap.read()
+                if frame.empty():
+                    break
+                out.write(frame)
+            out.release()
+            cap.release()
 
 
 def file2sticker(file_id: str, context: CallbackContext,
                  preprocess_type: FilePreprocessType = FilePreprocessType.default) -> BytesIO:
     file_bytes = context.bot.getFile(file_id).download_as_bytearray()
-    inp = np.asarray(bytearray(file_bytes), dtype=np.uint8)
-    image = cv2.imdecode(inp, cv2.IMREAD_COLOR)
+    image = None
+    if preprocess_type == FilePreprocessType.video_thumb:
+        file_bytes = context.bot.getFile(file_id).download_as_bytearray()
+        with tempfile.NamedTemporaryFile() as temp:
+            temp.write(file_bytes)
+
+            cap = cv2.VideoCapture(temp.name)
+            ret, image = cap.read()
+            cap.release()
+    else:
+        inp = np.asarray(bytearray(file_bytes), dtype=np.uint8)
+        image = cv2.imdecode(inp, cv2.IMREAD_COLOR)
+
+    if image is None:
+        return BytesIO()
     if preprocess_type == FilePreprocessType.circle:
         image = cv2.resize(image, (512, 512))
         image = cv2.cvtColor(image, cv2.COLOR_RGB2RGBA)
         thresh = np.load('mask.dat', allow_pickle=True)
         image = cv2.bitwise_and(image, image, mask=thresh)
-    elif preprocess_type == FilePreprocessType.default:
+    elif preprocess_type in [FilePreprocessType.default, FilePreprocessType.video_thumb]:
         h, w = image.shape[:2]
         if w >= h:
             new_h = int(h * (512 / w))
